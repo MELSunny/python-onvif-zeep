8ad1fec71a8ff6b2ba16f6473f36dc37290279b1
Yan Wen <yanwenchn@outlook.com>
2025-06-13 13:11:02 +0100

    Update definition.py
    change to offical wsdl folder structure

--- onvif/definition.py
+++ onvif/definition.py
@@ -1,19 +1,19 @@
 SERVICES = {
         # Name                              namespace                           wsdl file                      binding name
-        'devicemgmt'   : {'ns': 'http://www.onvif.org/ver10/device/wsdl',    'wsdl': 'devicemgmt.wsdl', 'binding' : 'DeviceBinding'},
-        'media'        : {'ns': 'http://www.onvif.org/ver10/media/wsdl',     'wsdl': 'media.wsdl',      'binding' : 'MediaBinding'},
-        'ptz'          : {'ns': 'http://www.onvif.org/ver20/ptz/wsdl',       'wsdl': 'ptz.wsdl',        'binding' : 'PTZBinding'},
-        'imaging'      : {'ns': 'http://www.onvif.org/ver20/imaging/wsdl',   'wsdl': 'imaging.wsdl',    'binding' : 'ImagingBinding'},
-        'deviceio'     : {'ns': 'http://www.onvif.org/ver10/deviceIO/wsdl',  'wsdl': 'deviceio.wsdl',   'binding' : 'DeviceIOBinding'},
-        'events'       : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'events.wsdl',     'binding' : 'EventBinding'},
-        'pullpoint'    : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'events.wsdl',     'binding' : 'PullPointSubscriptionBinding'},
-        'notification' : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'events.wsdl',     'binding' : 'NotificationProducerBinding'},
-        'subscription' : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'events.wsdl',     'binding' : 'SubscriptionManagerBinding'},
-        'analytics'    : {'ns': 'http://www.onvif.org/ver20/analytics/wsdl', 'wsdl': 'analytics.wsdl',  'binding' : 'AnalyticsEngineBinding'},
-        'recording'    : {'ns': 'http://www.onvif.org/ver10/recording/wsdl', 'wsdl': 'recording.wsdl',  'binding' : 'RecordingBinding'},
-        'search'       : {'ns': 'http://www.onvif.org/ver10/search/wsdl',    'wsdl': 'search.wsdl',     'binding' : 'SearchBinding'},
-        'replay'       : {'ns': 'http://www.onvif.org/ver10/replay/wsdl',    'wsdl': 'replay.wsdl',     'binding' : 'ReplayBinding'},
-        'receiver'     : {'ns': 'http://www.onvif.org/ver10/receiver/wsdl',  'wsdl': 'receiver.wsdl',   'binding' : 'ReceiverBinding'},
+        'devicemgmt'   : {'ns': 'http://www.onvif.org/ver10/device/wsdl',    'wsdl': 'ver10/device/wsdl/devicemgmt.wsdl', 'binding' : 'DeviceBinding'},
+        'media'        : {'ns': 'http://www.onvif.org/ver10/media/wsdl',     'wsdl': 'ver10/media/wsdl/media.wsdl',      'binding' : 'MediaBinding'},
+        'ptz'          : {'ns': 'http://www.onvif.org/ver20/ptz/wsdl',       'wsdl': 'ver20/ptz/wsdl/ptz.wsdl',        'binding' : 'PTZBinding'},
+        'imaging'      : {'ns': 'http://www.onvif.org/ver20/imaging/wsdl',   'wsdl': 'ver20/imaging/wsdl/imaging.wsdl',    'binding' : 'ImagingBinding'},
+        'deviceio'     : {'ns': 'http://www.onvif.org/ver10/deviceIO/wsdl',  'wsdl': 'ver10/deviceIO/wsdl/deviceio.wsdl',   'binding' : 'DeviceIOBinding'},
+        'events'       : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'ver10/events/wsdl/events.wsdl',     'binding' : 'EventBinding'},
+        'pullpoint'    : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'ver10/events/wsdl/events.wsdl',     'binding' : 'PullPointSubscriptionBinding'},
+        'notification' : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'ver10/events/wsdl/events.wsdl',     'binding' : 'NotificationProducerBinding'},
+        'subscription' : {'ns': 'http://www.onvif.org/ver10/events/wsdl',    'wsdl': 'ver10/events/wsdl/events.wsdl',     'binding' : 'SubscriptionManagerBinding'},
+        'analytics'    : {'ns': 'http://www.onvif.org/ver20/analytics/wsdl', 'wsdl': 'ver20/analytics/wsdl/analytics.wsdl',  'binding' : 'AnalyticsEngineBinding'},
+        'recording'    : {'ns': 'http://www.onvif.org/ver10/recording/wsdl', 'wsdl': 'ver10/recording/wsdl/recording.wsdl',  'binding' : 'RecordingBinding'},
+        'search'       : {'ns': 'http://www.onvif.org/ver10/search/wsdl',    'wsdl': 'ver10/search/wsdl/search.wsdl',     'binding' : 'SearchBinding'},
+        'replay'       : {'ns': 'http://www.onvif.org/ver10/replay/wsdl',    'wsdl': 'ver10/replay/wsdl/replay.wsdl',     'binding' : 'ReplayBinding'},
+        'receiver'     : {'ns': 'http://www.onvif.org/ver10/receiver/wsdl',  'wsdl': 'ver10/receiver/wsdl/receiver.wsdl',   'binding' : 'ReceiverBinding'},
         }
 
 #
